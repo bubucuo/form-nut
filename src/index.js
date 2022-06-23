5e305f25da63fee594771e3021b1ae708fea164a
gaoshaoyun <276185876@qq.com>
2022-06-23 15:48:49 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
+
 import "./index.less";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root")).render(<App />);
